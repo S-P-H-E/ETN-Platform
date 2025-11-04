11fd938d6e851743379958be1839b33fc9b1cf42
S-P-H-E <sphe.g.personal@gmail.com>
2025-11-04 21:34:18 +0200

    new ai feature

--- web/.next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"x5NdvKKyVPdFCzvS4kobrKQ2Snk9JRmlnPV6ECYzPdI=\"\n}"
--- web/.next/static/chunks/app_page_tsx_84e6549e._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS || (globalThis.TURBOPACK_CHUNK_LISTS = [])).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/node_modules_react-icons_fa_index_mjs_0459ff00._.js",
-  "static/chunks/node_modules_c94d8c35._.js",
-  "static/chunks/components_58c18231._.js"
-],
-    source: "dynamic"
-});
